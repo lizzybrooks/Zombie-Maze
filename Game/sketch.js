7dbecd416889d35b8270a414e24d2fb615a7d455
Tiffany <tiffanytang111@gmail.com>
2018-04-25 13:18:11 -0700

    moving canvas

--- Game/sketch.js
+++ Game/sketch.js
@@ -1,49 +1,37 @@
+let x = 0;
+let y = 0;
+let z = 0;
+let y2 = 0;
+
 function setup() {
-  createCanvas(800, 600);
-  background(200);
-  city();
-  car();
+  //createCanvas(1000, 500);
 }
 
 function draw() {
-  //white background rectangle
-  noStroke();
-  fill(255);
-  rect(0, 0, 75, 20);
-  //text showing mouse coordinates
-  fill(255, 0, 0);
-  text("("+mouseX + ", " + mouseY+")", 5, 15);
+  createCanvas(100 + x, 500);
+  background("blue");
+  rect(50,50, 60 + z, 60 + y2);
+  rect(50+x,50+y,50,50);
+  rect(200,50,50,50);
+  //function keyPressed();
 }
 
-function keyPressed(){
-if (keyCode === LEFT_ARROW){
-    car();
-    translate(20,0);
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW){
+    x = x + 30;
+    z = z + 30;
   }
-  else if (keyCode === RIGHT_ARROW){
-    car();
-    translate(-20,0);
+  else if (keyCode === LEFT_ARROW){
+    x = x - 30;
   }
 }
 
-function city(){
-  strokeWeight(30);
-  line(0,0,200,0);
-  line(0,100,200,100);
-  line(300,0,300,100);
-  line(300,100,500,100);
-  line(600,0,600,100);
-  line(600,100,700,100);
-  line(100,200,500,200);
-  line(500,200,500,300);
-  line(600,200,600,300);
-  line(700,200,700,270);
-  line(700,270,800,270);
-  line(0,300,200,300);
-}
-
-function car(){
-  strokeWeight(10);
-  stroke('blue');
-  ellipse(20,50,10,10);
-}
+//function keyPressed() {
+  //if (keyCode === DOWN_ARROW){
+    //y = y + 30;
+    //y2 = y2 + 30;
+  //}
+  //else if (keyCode === UP_ARROW){
+    //y = y - 30;
+//  }
+//}
