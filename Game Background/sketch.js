737ed87a2e72b651b3f36913e2f24aa90fb26dd8
Tiffany <tiffanytang111@gmail.com>
2018-04-30 11:20:55 -0700

    Working on canvas background

--- Game Background/sketch.js
+++ Game Background/sketch.js
@@ -2,36 +2,53 @@ let x = 0;
 let y = 0;
 let z = 0;
 let y2 = 0;
+let canvasx = 500+x;
+let canvasy = 500+y;
 
 function setup() {
-  //createCanvas(1000, 500);
 }
 
 function draw() {
-  createCanvas(100 + x, 500);
+  createCanvas(canvasx, canvasy);
   background("red");
+  fill("pink");
   rect(50,50, 60 + z, 60 + y2);
-  rect(50+x,50+y,50,50);
+  fill("blue");
   rect(200,50,50,50);
   //function keyPressed();
 }
 
-function keyPressed() {
-  if (keyCode === RIGHT_ARROW){
-    x = x + 30;
-    z = z + 30;
-  }
-  else if (keyCode === LEFT_ARROW){
-    x = x - 30;
-  }
-}
+// function keyPressed() {
+//   if (keyCode === RIGHT_ARROW){
+//     x = x + 30;
+//     z = z + 30;
+//   }
+//   else if (keyCode === LEFT_ARROW){
+//     x = x - 30;
+//   }
+// }
 
+class sprite {
+  drawSprite(){
+        fill("black");
+    		rect(50,50,50,50);
+	}
+
+	//update the location of the ball, so it moves across the screen
+	moveSprite(){
+		 if(keyIsDown(RIGHT_ARROW)){
+       this.x = this.x + 10;
+     }
+   }
+
+
+}
 //function keyPressed() {
   //if (keyCode === DOWN_ARROW){
-    //y = y + 30;
-    //y2 = y2 + 30;
-  //}
-  //else if (keyCode === UP_ARROW){
-    //y = y - 30;
+  //  y = y + 30;
+//    y2 = y2 + 30;
 //  }
+//  else if (keyCode === UP_ARROW){
+  //  y = y - 30;
+  //}
 //}
