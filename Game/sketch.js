579d970795ea6a43822ab32c394a12d09e64d3e3
AJ Wu <ajwu@AJs-MacBook-Pro.attlocal.net>
2018-05-03 20:46:47 -0700

    finished the rest of the maze background

--- Game/sketch.js
+++ Game/sketch.js
@@ -2,7 +2,7 @@ let x = 100;
 let y = 100;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1000, 1000);
   background(200);
   city(x,y);
   car();
@@ -22,15 +22,11 @@ function draw() {
 function keyPressed(){
 if (keyCode === LEFT_ARROW){
 
-    translate(x = x + 20,0);
-    background(200);
-city(x,y);
+
   }
   else if (keyCode === RIGHT_ARROW){
 
-    translate(x = x - 20,0);
-    background(200);
-    city(x,y);
+
   }
 }
 
@@ -67,18 +63,51 @@ city(x,y);
 function city(x,y){//make all cordinates x100,y100
   strokeWeight(30);
   stroke('black')
-  line(x-100,y-100,x+100,y-100);
+  //top
+  line(x-100,y-100,x+900,y-100);//top side line
+  line(x+900,y-100,x+900,y+900);//right side line
   line(x-100,y,x+100,y);
   line(x+200,y-100,x+200,y);
-  line(x+200,y,x+400,y);
-  line(x+500,y-100,x+500,y);
-  line(x+500,y,x+600,y);
-  line(x,y+100,x+400,y+100);
-  line(x+400,y+100,x+400,y+200);
-  line(x+500,y+100,x+500,y+200);
-  line(x+600,y+100,x+600,y+170);
-  line(x+600,y+170,x+700,y+170);
+  line(x+200,y,x+450,y);
+  line(x+570,y-100,x+570,y);
+  line(x+570,y,x+800,y);
+  line(x,y+100,x+600,y+100);
+  line(x+600,y+100,x+600,y+250);
+  line(x+700,y+100,x+700,y+250);
+  line(x+700,y+250,x+800,y+250);
+  line(x+800,y+250,x+800,y+500);
+  line(x+800,y+500,x+700,y+500);
+//part C
+  line(x+800,y+100,x+800,y+170);
+  line(x+800,y+170,x+900,y+170);
   line(x-100,y+200,x+100,y+200);
+  line(x-100,y-100,x-100,y+900);//left side line
+  line(200,300,200,450);
+  line(270,450,100,450);
+  line(100,450,100,390);
+  line(270,550,100,550);
+//part B
+  line(500,200,500,450);
+  line(500,450,800,450);
+  line(800,450,800,1000);
+  line(300,300,400,300);
+  line(400,300,400,550);
+  line(400,550,700,550);
+  line(700,550,700,750);
+  //part A
+  line(0,650,450,650);
+  line(400,650,400,800);
+  line(700,750,600,750);
+  line(600,750,600,1000);
+  line(700,860,700,1000);
+  line(1000,700,900,700);
+  line(800,800,900,800);
+  line(1000,1000,800,1000);
+  line(0,1000,600,1000);
+  line(500,800,500,1000);
+  line(300,800,300,1000);
+  line(300,800,100,800);
+
 }
 
 function car(x,y){
